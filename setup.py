67805dff8ab9ea53917bf616ef4297f5b52e3ec8
Chris Loew <cloew123@gmail.com>
2014-04-18 20:51:01 -0600

    Fixed package references in setup install file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-setup(name='pbf.python.unittest',
+setup(name='pbf_python_unittest',
       version='.2',
       description="Programmer's Best Friend Utility Extension for the Python Unittest Library",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
-      packages=['pbf.python', 'pbf.python.unittest', 'pbf.python.unittest.Commands', 
-                'pbf.python.unittest.helpers', 'pbf.python.unittest.templates'],
-      #package_data = {'pbf.python.unittest.templates':[]}, # Add template files
+      packages=['pbf_python_unittest', 'pbf_python_unittest.Commands', 
+                'pbf_python_unittest.helpers', 'pbf_python_unittest.templates'],
+      #package_data = {'pbf_python_unittest.templates':[]}, # Add template files
      )
